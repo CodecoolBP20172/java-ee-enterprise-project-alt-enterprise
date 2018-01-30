97942873b27135834fe571a189bc0addd508f04d
Akos Putnoki <putnokiakos@gmail.com>
2018-01-30 15:49:54 +0100

    Lists added to Guest and Owner

--- src/main/java/com/codecool/restauratio/models/users/Guest.java
+++ src/main/java/com/codecool/restauratio/models/users/Guest.java
@@ -1,9 +1,18 @@
 package com.codecool.restauratio.models.users;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Guest extends User{
+
+    // annotate to reservation and order tables with OneToMany
+    private List<Reservation> reservations;
+    private List<Order> orders;
 
     public Guest(String userName, String password) {
         super(userName, password, AccessRights.GUEST);
+        reservations = new ArrayList<Reservation>();
+        orders = new ArrayList<Order>();
     }
 
 }
--- src/main/java/com/codecool/restauratio/models/users/Owner.java
+++ src/main/java/com/codecool/restauratio/models/users/Owner.java
@@ -1,9 +1,18 @@
 package com.codecool.restauratio.models.users;
 
+import com.codecool.restauratio.models.Restaurant;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Owner extends User{
+
+    // annotate with OneToMany to restaurant table
+    private List<Restaurant> restaurants;
 
     public Owner(String userName, String password) {
         super(userName, password, AccessRights.OWNER);
+        restaurants = new ArrayList<Restaurant>();
     }
 
 
