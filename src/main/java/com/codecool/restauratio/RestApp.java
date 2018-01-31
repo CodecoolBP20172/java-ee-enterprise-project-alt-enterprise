c854cac48f5790d278e98b46b65215c816c40b96
Gergely Papi <gergely.papi.cc@gmail.com>
2018-01-31 09:46:31 +0100

    Test environment for db connection

--- src/main/java/com/codecool/restauratio/RestApp.java
+++ src/main/java/com/codecool/restauratio/RestApp.java
@@ -1,7 +1,27 @@
 package com.codecool.restauratio;
 
-public class RestApp {
-    public static void main(String[] args) {
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 
+public class RestApp {
+
+    public static void populateDb(EntityManager em) {
+
+        EntityTransaction transaction = em.getTransaction();
+
+        //instantiate objects    
+
+        transaction.begin();
+        //persist objects em.persist(object)
+        transaction.commit();
+    }
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpaexamplePU");
+        EntityManager em = emf.createEntityManager();
+
+        populateDb(em);
     }
 }
