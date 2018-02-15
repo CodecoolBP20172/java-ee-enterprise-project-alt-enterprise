f5a20cf065aafc4017b7840495b19214bc2b066e
Gergely Papi <gergely.papi.cc@gmail.com>
2018-02-15 11:44:47 +0100

    Added namedquery for restaurant search based on location, parameter is given

--- src/main/java/com/codecool/restauratio/dao/RestaurantDao.java
+++ src/main/java/com/codecool/restauratio/dao/RestaurantDao.java
@@ -6,6 +6,7 @@ import com.codecool.restauratio.models.Restaurant;
 import com.codecool.restauratio.utils.DatabaseUtility;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.util.List;
 
 public class RestaurantDao {
@@ -24,6 +25,18 @@ public class RestaurantDao {
     public Restaurant getById(long id) throws ConnectToDBFailed {
         try {
             return em.find(Restaurant.class, id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ConnectToDBFailed(e.getMessage());
+        }
+    }
+
+    public List<Restaurant> getByLocation(String location) throws ConnectToDBFailed {
+        try {
+            System.out.println(location);
+            Query query = em.createNamedQuery("getRestaurantsByLocation");
+            query.setParameter("location", location);
+            return (List<Restaurant>) query.getResultList();
         } catch (Exception e) {
             e.printStackTrace();
             throw new ConnectToDBFailed(e.getMessage());
