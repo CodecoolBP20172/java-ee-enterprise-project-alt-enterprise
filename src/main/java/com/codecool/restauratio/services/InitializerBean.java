b5e8acae5d3fbfb241a0af408965221b586bca61
Debora Csomos <csomidebi@gmail.com>
2018-02-28 17:34:01 +0100

    Added plus sample data.

--- src/main/java/com/codecool/restauratio/services/InitializerBean.java
+++ src/main/java/com/codecool/restauratio/services/InitializerBean.java
@@ -1,18 +1,24 @@
 package com.codecool.restauratio.services;
 
 import com.codecool.restauratio.models.Food;
+import com.codecool.restauratio.models.Reservation;
 import com.codecool.restauratio.models.Restaurant;
 import com.codecool.restauratio.models.users.User;
 import com.codecool.restauratio.repository.FoodRepository;
+import com.codecool.restauratio.repository.ReservationRepository;
 import org.springframework.stereotype.Component;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Component
 public class InitializerBean {
 
-    public InitializerBean(UserService userService, RestaurantService restaurantService, FoodRepository foodrepo) {
+    public InitializerBean(UserService userService, RestaurantService restaurantService,
+                           ReservationRepository reservRepo, FoodRepository foodRepo) throws ParseException {
 
         User user1 = new User("józsi", "József", "Bende", "hurka", "józsi@citromail.com", "codecool", true, false);
         User user2 = new User("bodri", "Bodri", "Nemecsek", "mecska", "bodri@gmail.com", "home", true, true);
@@ -20,9 +26,9 @@ public class InitializerBean {
         Food f = new Food("Melák Menü", 1500, "szenya, rántotthus, rántottsajt", "király");
         Food f2 = new Food("buja burger", 200, "burger", "jó");
         Food f3 = new Food("Tele Tál Falafel", 4000, "minden ami blefér", "ragya");
-        foodrepo.save(f);
-        foodrepo.save(f2);
-        foodrepo.save(f3);
+        foodRepo.save(f);
+        foodRepo.save(f2);
+        foodRepo.save(f3);
 
         List<Food> list = new ArrayList<>();
         list.add(f);
@@ -42,5 +48,10 @@ public class InitializerBean {
         restaurantService.addRestaurant(r);
         restaurantService.addRestaurant(r2);
         restaurantService.addRestaurant(r3);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        Date date = sdf.parse("2018-03-01 11:45");
+        Reservation reservation = new Reservation(date, 4, r2, user1);
+        reservRepo.save(reservation);
     }
 }
