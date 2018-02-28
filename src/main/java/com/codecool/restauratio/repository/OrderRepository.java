905cc0d66b04b1a53554143518bcc3ca9ef8eb16
Debora Csomos <csomidebi@gmail.com>
2018-02-28 22:21:55 +0100

    Added new query.

--- src/main/java/com/codecool/restauratio/repository/OrderRepository.java
+++ src/main/java/com/codecool/restauratio/repository/OrderRepository.java
@@ -1,7 +1,15 @@
 package com.codecool.restauratio.repository;
 
 import com.codecool.restauratio.models.Order;
+import com.codecool.restauratio.models.users.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+
+    @Query("SELECT o FROM Order o WHERE user_id = :userId")
+    List<Order> findAllByUser(@Param("userId") int userId);
 }
