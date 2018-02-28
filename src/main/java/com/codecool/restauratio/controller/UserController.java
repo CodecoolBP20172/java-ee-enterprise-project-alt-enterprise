fc60a2870810cb715cb54861d2f273900538d1ca
Debora Csomos <csomidebi@gmail.com>
2018-02-28 17:32:30 +0100

    Extended renderUserPage method.

--- src/main/java/com/codecool/restauratio/controller/UserController.java
+++ src/main/java/com/codecool/restauratio/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.codecool.restauratio.controller;
 
 import com.codecool.restauratio.customException.AccessForbiddenException;
+import com.codecool.restauratio.models.Order;
+import com.codecool.restauratio.models.Reservation;
 import com.codecool.restauratio.models.users.User;
+import com.codecool.restauratio.repository.OrderRepository;
+import com.codecool.restauratio.repository.ReservationRepository;
 import com.codecool.restauratio.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,12 +15,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private ReservationRepository reservationRepository;
+    @Autowired
+    private OrderRepository orderRepository;
 
     @RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
     public String renderUserPage(@PathVariable(value = "id") int id,
@@ -26,7 +35,11 @@ public class UserController {
             throw new AccessForbiddenException();
         }
         User user = userRepository.findOne(id);
+        List<Reservation> reservations = reservationRepository.findAllByUser(user.getUserId());
+        List<Order> orders = orderRepository.getAllByUser(user);
         model.addAttribute("user", user);
+        model.addAttribute("reservations", reservations);
+        model.addAttribute("orders", orders);
         model.addAttribute("loggedin", true);
         return "userpage";
     }
