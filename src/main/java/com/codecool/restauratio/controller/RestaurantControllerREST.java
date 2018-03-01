8f4862c1c176bac83e6f01860eb9cfbc10359800
Soma Szolyák <soma.szolyak@gmail.com>
2018-03-01 10:03:42 +0100

    deleted reservationControllerRest and moved it's methods to reservationControllerREST

--- src/main/java/com/codecool/restauratio/controller/RestaurantControllerREST.java
+++ src/main/java/com/codecool/restauratio/controller/RestaurantControllerREST.java
@@ -1,5 +1,6 @@
 package com.codecool.restauratio.controller;
 
+import com.codecool.restauratio.customException.ConnectToDBFailed;
 import com.codecool.restauratio.models.Restaurant;
 import com.codecool.restauratio.services.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+import java.sql.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -29,6 +32,14 @@ public class RestaurantControllerREST {
         return new ResponseEntity<>(restService.restaurantTypeBrowser(data.get("description")), HttpStatus.OK);
     }
 
+    @RequestMapping(value = "/api/make_reservation", method = RequestMethod.POST)
+    public ResponseEntity<String> makeReservation(@RequestBody Map<String, String> data, HttpSession session) throws ConnectToDBFailed {
+        int userId = (int) session.getAttribute("id");
+        //System.out.println(userId);
+        restService.makeReservation(Date.valueOf(data.get("date")),
+                Integer.valueOf(data.get("numOfPeople")), Integer.valueOf(data.get("restaurantId")), userId, data.get("comment"));
+        return new ResponseEntity<>("success", HttpStatus.OK);
+    }
 
 
 }
